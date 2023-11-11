ec86f2e53f08ff323ea7e50866dbaec2e030f984
Hemant Kumar Singh <hemantsinghultimate@gmail.com>
2023-11-11 11:44:07 +0530

    updated

--- index.js
+++ index.js
@@ -37,12 +37,25 @@ io.on('connection', (socket) => {
             roomUsers[room] = [];
         }
 
+        // Add user to the roomUsers list for the specific room
         roomUsers[room].push(username);
 
+        // Update user list for the specific room
         io.to(room).emit('update user list', roomUsers[room]);
+
+        // Notify others in the room about the new user
+        socket.to(room).emit('chat message', `${username} has joined the room`);
     });
 
     socket.on('disconnect', () => {
+        // Remove the user from the roomUsers list when disconnected
+        Object.keys(roomUsers).forEach(room => {
+            roomUsers[room] = roomUsers[room].filter(user => user !== socket.username);
+        });
+
+        // Notify others in the room about the user's disconnection
+        socket.to(socket.room).emit('chat message', `${socket.username} has left the room`);
+
         console.log('user disconnected');
     });
 });
@@ -60,6 +73,8 @@ app.post('/', (req, res) => {
 app.get('/room', (req, res) => {
     var username = req.query.username;
     var room = req.query.room;
+
+    // Set username and room as properties of the socket for later use
     res.render('room', { username, room });
 });
 
@@ -68,5 +83,5 @@ app.get('/logout', (req, res) => {
 });
 
 server.listen(PORT, function () {
-    console.log('listening on 3000');
+    console.log(`listening on ${PORT}`);
 });
