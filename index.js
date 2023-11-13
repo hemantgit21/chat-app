dc09f3858d127a0609525135e802af1fa2ba18db
hemantgit21 <119285619+hemantgit21@users.noreply.github.com>
2023-11-14 04:45:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
-
 const roomUsers = {};
 
 let uname;
